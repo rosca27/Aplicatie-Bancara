5bbad59bb95fd472ae5167e1967beb7f49620016
rosca27 <roscasergiu74@yahoo.com>
2021-12-30 13:30:37 +0200

    Update LichidareCont.java

--- src/LichidareCont.java
+++ src/LichidareCont.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class LichidareCont {
 
@@ -18,12 +21,23 @@ public class LichidareCont {
         frame4.setVisible(true);
         lista_conturi.setBounds(50,80,400,200);
         lichidare.setBounds(200,300,100,50);
+        lichidare.setFocusable(false);
         alege.setBounds(200,20,200,30);
         alege.setFont(new Font("Times New Roman",Font.PLAIN,18));
         frame4.setResizable(false);
         frame4.add(alege);
         frame4.add(lichidare);
         frame4.add(lista_conturi);
+
+        lichidare.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource() == lichidare){
+
+
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
